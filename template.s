b55ff9f44f65145a273cc4eddd4da9af8f2e4d86
Matt Fetterman <navx2810@gmail.com>
2016-10-04 02:01:49 -0400

    changed the template to include the appropriate comments, and changed the makefile to prompt yoas with a message about the compiler

--- template.s
+++ template.s
@@ -1,10 +1,14 @@
+# Program:	<< WHAT DO I DO >>
+# Author:	Matt Fetterman
+# Date:		XX/XX/2016
+
 .section .data
 
 .section .text
 .globl _start
 
 _start:
-	call EndProg	
+	call EndProg
 
 EndProg:
 	movl	$1,	%eax
